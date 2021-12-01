9c0b1be481a39fad8f9f20ab93ff3d2df3f85d37
Kyle Travis <kyle.travis@discordapp.com>
2021-12-01 11:33:37 -0500

    cleanup

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -3,23 +3,25 @@ use itertools::Itertools;
 fn main() {
     let input = include_str!("../../inputs/day1.txt");
 
-    /* part 1 */
-    let count: usize = input
-        .lines()
-        .map(|line| line.parse::<i64>().unwrap())
-        .tuple_windows()
-        .map(|(a, b)| if a < b { 1 } else { 0 })
-        .sum();
-    println!("part 1: {}", count);
+    println!(
+        "part 1: {}",
+        input
+            .lines()
+            .map(|line| line.parse::<i64>().unwrap())
+            .tuple_windows()
+            .filter(|(a, b)| a < b)
+            .count()
+    );
 
-    /* part 2 */
-    let count: usize = input
-        .lines()
-        .map(|line| line.parse::<i64>().unwrap())
-        .tuple_windows()
-        .map(|(a, b, c)| a+b+c)
-        .tuple_windows()
-        .map(|(a, b)| if a < b { 1 } else { 0 })
-        .sum();
-    println!("part 2: {}", count);
+    println!(
+        "part 2: {}",
+        input
+            .lines()
+            .map(|line| line.parse::<i64>().unwrap())
+            .tuple_windows()
+            .map(|(a, b, c)| a + b + c)
+            .tuple_windows()
+            .filter(|(a, b)| a < b)
+            .count()
+    );
 }
